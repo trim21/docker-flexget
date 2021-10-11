95d5f48f6602dc0ed6ce337de99f1149e216ab98
Trim21 <trim21.me@gmail.com>
2021-10-11 13:50:59 +0800

    update

--- build.js
+++ build.js
@@ -11,12 +11,16 @@ async function main() {
   try {
     await exec("docker", ["pull", baseImage]);
   } catch {
-    await exec("docker", [
-      "build",
-      `https://github.com/Flexget/Flexget.git#v${FLEXGET_VERSION}`,
-      "--tag",
-      baseImage,
-    ]);
+    await exec(
+      "docker",
+      [
+        "build",
+        `https://github.com/Flexget/Flexget.git#v${FLEXGET_VERSION}`,
+        "--tag",
+        baseImage,
+      ],
+      { silent: true }
+    );
     await exec("docker", ["push", baseImage]);
   }
   await exec("docker", ["tag", baseImage, "flexget-base:latest"]);
