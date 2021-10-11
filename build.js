c8cee8ab3bd1923bfc17d273f8c76dee45c7a4a5
Trim21 <trim21.me@gmail.com>
2021-10-11 14:03:28 +0800

    update

--- build.js
+++ build.js
@@ -2,45 +2,31 @@ const { exec } = require("@actions/exec");
 const fs = require("fs");
 
 async function main() {
-  const FLEXGET_VERSION = fs
-    .readFileSync("./requirements.txt")
-    .toString()
-    .trim()
-    .split("==")
-    .pop();
+  const FLEXGET_VERSION = fs.readFileSync("./requirements.txt").toString().trim().split("==").pop();
 
   const baseImage = `ghcr.io/trim21/flexget:base-${FLEXGET_VERSION}`;
+  const remote = `https://github.com/Flexget/Flexget.git#v${FLEXGET_VERSION}`;
+  const silent = { silent: true };
+
   try {
-    await exec("docker", ["pull", baseImage], { silent: true });
+    await exec("docker", ["pull", baseImage], silent);
   } catch {
-    await exec(
-      "docker",
-      [
-        "build",
-        `https://github.com/Flexget/Flexget.git#v${FLEXGET_VERSION}`,
-        "--tag",
-        baseImage,
-      ],
-      { silent: true }
-    );
-    await exec("docker", ["push", baseImage], { silent: true });
+    await exec("docker", ["build", remote, "--tag", baseImage], silent);
+    await exec("docker", ["push", baseImage], silent);
   }
+
   await exec("docker", ["tag", baseImage, "flexget-base:latest"]);
+  await exec("docker", ["build", "--tag", "flexget:current", "."], silent);
 
-  await exec("docker", ["build", "--tag", "flexget:current", "."], {
-    silent: true,
-  });
-
-  const tag = FLEXGET_VERSION;
-  const [major, minor, _] = tag.split(".");
-  const versions = ["latest", tag, `${major}.${minor}`, major];
+  const [major, minor, _] = FLEXGET_VERSION.split(".");
+  const versions = ["latest", major, `${major}.${minor}`, tag];
 
   console.log(versions);
 
   for (const version of versions) {
     const dst = `ghcr.io/trim21/flexget:${version}`;
     await exec("docker", ["tag", "flexget:current", dst]);
-    await exec("docker", ["push", dst], { silent: true });
+    await exec("docker", ["push", dst], silent);
   }
 }
 
