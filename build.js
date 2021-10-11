3fa9d8128fcc4089173ac397840f86199b427c51
Trim21 <trim21.me@gmail.com>
2021-10-11 13:46:31 +0800

    fix

---
+++ build.js
@@ -0,0 +1,35 @@
+const { exec } = require("@actions/exec");
+const github = require("@actions/github");
+
+const context = github.context;
+const ref = context.ref;
+
+const FLEXGET_VERSION = process.env.VERSION;
+
+async function main() {
+  const baseImage = `flexget:base-${FLEXGET_VERSION}`;
+  try {
+    await exec("docker", ["pull", baseImage]);
+  } catch {
+    await exec(
+      `docker build "https://github.com/Flexget/Flexget.git#v${FLEXGET_VERSION}" --tag flexget:base-${FLEXGET_VERSION}`
+    );
+  }
+  await exec("docker", ["tag", baseImage, "flexget-base:latest"]);
+
+  await exec("docker", ["build", "--tag", "flexget:current", "."]);
+
+  const tag = FLEXGET_VERSION;
+  const [major, minor, _] = tag.split(".");
+  const versions = ["latest", tag, `${major}.${minor}`, major];
+
+  console.log(versions);
+
+  for (const version of versions) {
+    const dst = `ghcr.io/${context.actor.toLowerCase()}/flexget:${version}`;
+    await exec("docker", ["tag", "flexget:current", dst]);
+    await exec("docker", ["push", dst]);
+  }
+}
+
+main();
